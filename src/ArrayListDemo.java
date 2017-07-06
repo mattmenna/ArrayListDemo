b869697d7d778738df7a45085a8b166a72a3b9e5
Matthew Menna <mattmenna@gmail.com>
2017-07-06 11:41:55 -0400

    Initial commit

---
+++ src/ArrayListDemo.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Matt on 7/6/2017.
+ */
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //declare an array of strings
+        ArrayList<String> list1 = new ArrayList<String>(5);
+        list1.add("Matt");
+        list1.add("Eric");
+        list1.add("Mark");
+        list1.add("Vernon");
+
+        System.out.println(list1);
+    }
+}
